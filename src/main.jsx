d720278c766d9f15378af22462ca0af40230488b
mohammadibrahim20 <ibinfo2017bd@gmail.com>
2023-10-27 20:15:08 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import "./index.css";
+import router from "./routes/router";
+
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <App />,
+//   },
+// ]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+    
+  </React.StrictMode>
+);
